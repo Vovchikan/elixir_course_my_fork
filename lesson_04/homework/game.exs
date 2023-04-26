6f593aef05403f23323b553b7325f199d15b5899
Vladimir Samorodov <vovchikan@gmail.com>
2023-04-26 05:59:33 +0000

    lesson4

--- lesson_04/homework/game.exs
+++ lesson_04/homework/game.exs
@@ -1,19 +1,46 @@
 defmodule Game do
+  @type role()  :: :admin | :moderator | atom()
+  @type age()   :: integer()
+  @type name()  :: String.t()
 
+  @spec join_game({:user, name, age, role}) ::
+          :ok | :error
   def join_game(user) do
-    # TODO add your implementation
+    with {:user, _name, age, role} <- user do
+      case {role, age} do
+        {r, _} when r == :admin or r == :moderator -> :ok
+        {_, a} when a >= 18                      -> :ok
+        _                                          -> :error
+      end
+    else
+      _ -> :error
+    end
   end
 
-  def move_allowed?(current_color, figure) do
-    # TODO add your implementation
+  @type type() :: :pawn | :rock | :bishop | :knight | :queen | :king
+  @type color() :: :white | :black
+  @type figure() :: {type, color}
+
+  @spec move_allowed?(color, figure) :: boolean
+  def move_allowed?(current_color, {type, color}) do
+    (type == :pawn || type == :rock) && color == current_color
   end
 
+  @spec single_win?(boolean, boolean) :: boolean
   def single_win?(a_win, b_win) do
-    # TODO add your implementation
+    (a_win != b_win) && (a_win || b_win)
   end
 
+  @spec double_win?(boolean, boolean, boolean) ::
+          :ab | :ac | :bc | false
   def double_win?(a_win, b_win, c_win) do
-    # TODO add your implementation
+    case {a_win, b_win, c_win} do
+      {true, true, true} -> false
+      {true, true, _} -> :ab
+      {true, _, true} -> :ac
+      {_, true, true} -> :bc
+      _               -> false
+    end
   end
 
 end
--- lesson_04/homework/tic_tac_toe.exs
+++ lesson_04/homework/tic_tac_toe.exs
@@ -1,18 +1,85 @@
 defmodule TicTacToe do
 
   @type cell :: :x | :o | :f
+  defguard is_cell(val) when val in [:x, :o, :f]
   @type row :: {cell, cell, cell}
   @type game_state :: {row, row, row}
   @type game_result :: {:win, :x} | {:win, :o} | :no_win
 
   @spec valid_game?(game_state) :: boolean
   def valid_game?(state) do
-    # TODO add your implementation
+    with {_, _, _} <- state do
+      Tuple.to_list(state)
+      |> Enum.all?(fn row -> valid_row?(row) end)
+    else
+      _ -> false
+    end
+  end
+
+  defp valid_row?(row) do
+    with {_, _, _} <- row do
+      Tuple.to_list(row)
+      |> Enum.all?(fn cell ->
+        IO.inspect(cell, label: "CELL")
+        IO.inspect(is_cell(cell), label: "is_cell()")
+        is_cell(cell)
+      end)
+    else
+      _ -> false
+    end
   end
 
   @spec check_who_win(game_state) :: game_result
   def check_who_win(state) do
-    # TODO add your implementation
+    with true <- valid_game?(state) do
+      case state do
+        {
+          {val, _, _},
+          {_, val, _},
+          {_, _, val}
+        } when val in [:x, :o] -> {:win, val}
+        {
+          {_, _, val},
+          {_, val, _},
+          {val, _, _}
+        } when val in [:x, :o] -> {:win, val}
+        {
+          {val, _, _},
+          {val, _, _},
+          {val, _, _}
+        } when val in [:x, :o] -> {:win, val}
+        {
+          {_, val, _},
+          {_, val, _},
+          {_, val, _}
+        } when val in [:x, :o] -> {:win, val}
+        {
+          {_, _, val},
+          {_, _, val},
+          {_, _, val}
+        } when val in [:x, :o] -> {:win, val}
+        {
+          {val, val, val},
+          {_, _, _},
+          {_, _, _}
+        } when val in [:x, :o] -> {:win, val}
+        {
+          {_, _, _},
+          {val, val, val},
+          {_, _, _}
+        } when val in [:x, :o] -> {:win, val}
+        {
+          {_, _, _},
+          {_, _, _},
+          {val, val, val}
+        } when val in [:x, :o] -> {:win, val}
+        _ -> :no_win
+      end
+    else
+      v ->
+        IO.inspect(v, label: "I'm here")
+        :no_win
+    end
   end
 
 end
